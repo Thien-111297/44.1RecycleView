78e2d86252b3f0c804dbf736794d0202ed4a7b6f
Admin <hubtth07@gmail.com>
2020-08-13 22:55:53 +0700

    Initial commit

---
+++ app/src/main/java/kot/android/a441recycleview/ListAdapterWithRecycleView.java
@@ -0,0 +1,159 @@
+package kot.android.a441recycleview;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ListAdapterWithRecycleView  extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final String TAG = ListAdapterWithRecycleView.class.getSimpleName();
+
+    public interface PersonModifier{
+        public void onPersonSelected(int position);
+        public void onPersonDeleted(int position);
+    }
+
+    private List<Object> catalogue;
+    private Context context;
+
+    private PersonModifier personModifier;
+
+    public ListAdapterWithRecycleView(Context context,List<Object> catalogue){
+        this.catalogue = catalogue;
+        this.context=context;
+    }
+
+    public void setPersonModifier(PersonModifier personModifier){
+        this.personModifier = personModifier;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(catalogue.get(position) instanceof Advertisement){
+            return R.layout.layout_advertisement_row_item;
+        } else if (catalogue.get(position) instanceof Person) {
+            return R.layout.layout_person_row_item;
+        }else{
+            return -1;
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
+        final RecyclerView.ViewHolder holder;
+        View view;
+        switch (viewType){
+            case R.layout.layout_advertisement_row_item:
+                view= LayoutInflater.from(context).inflate(R.layout.layout_advertisement_row_item,parent,false);
+                holder=new AdvertisementHolder(view);
+                break;
+
+            case R.layout.layout_person_row_item:
+                view= LayoutInflater.from(context).inflate(R.layout.layout_person_row_item,parent,false);
+                holder=new PersonViewHolder(view);
+                view.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View view) {
+                        int position=holder.getAdapterPosition();
+                        Toast.makeText(context,"Item at position "+position+" deleted",Toast.LENGTH_SHORT).show();
+                        catalogue.remove(position);
+                        notifyDataSetChanged();
+                        if(personModifier!=null){personModifier.onPersonDeleted(position);}
+                        return true;
+                    }
+                });
+                view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(personModifier!=null){
+                            personModifier.onPersonSelected(holder.getAdapterPosition());
+                        }
+                    }
+                });
+                break;
+            default:
+                view= LayoutInflater.from(context).inflate(R.layout.layout_advertisement_row_item,parent,false);
+                holder=new AdvertisementHolder(view);
+                break;
+
+        }
+
+        return holder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof PersonViewHolder){
+            final Person person= (Person) catalogue.get(position);
+            ((PersonViewHolder)holder).textViewName.setText(person.getName());
+            ((PersonViewHolder)holder).textViewLastName.setText(person.getLastName());
+            ((PersonViewHolder)holder).textViewNationality.setText(person.getNationality());
+            ((PersonViewHolder)holder).textViewGender.setText(person.getGender()== Person.GENDER.MALE?"Male":"Female");
+            ((PersonViewHolder)holder).textViewGender.setEnabled(true);
+            ((PersonViewHolder)holder).textViewGender.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Person.GENDER  gender= ((Person)catalogue.get(holder.getAdapterPosition())).getGender();
+                    if(gender == Person.GENDER.MALE){
+                        ((Person)catalogue.get(holder.getAdapterPosition())).setGender(Person.GENDER.FEMALE);
+                    }else if(gender == Person.GENDER.FEMALE){
+                        ((Person)catalogue.get(holder.getAdapterPosition())).setGender(Person.GENDER.MALE);
+                    }
+                    notifyItemChanged(holder.getAdapterPosition());
+                }
+            });
+        }else if(holder instanceof AdvertisementHolder){
+            ((AdvertisementHolder)holder).textViewAdvertMessage.setText(((Advertisement)catalogue.get(position)).getAdMessage());
+            ((AdvertisementHolder)holder).textViewAdvertMessage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(context,"Advertisement not available",Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        Log.i(TAG,"onBindViewHolder invoked: "+position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return catalogue.size();
+    }
+
+    class PersonViewHolder extends RecyclerView.ViewHolder{
+        public TextView textViewName;
+        public TextView textViewLastName;
+        public TextView textViewGender;
+        public TextView textViewNationality;
+
+        public PersonViewHolder(View view){
+            super(view);
+            textViewName = (TextView)view.findViewById(R.id.textViewName);
+            textViewLastName=(TextView)view.findViewById(R.id.textViewLastName);
+            textViewGender = (TextView)view.findViewById(R.id.textViewGender);
+            textViewNationality = (TextView)view.findViewById(R.id.textViewNationality);
+        }
+    }
+
+    class AdvertisementHolder extends RecyclerView.ViewHolder{
+        public ImageView imageViewAdvertisementBanner;
+        public TextView textViewAdvertMessage;
+
+        public AdvertisementHolder(View view){
+            super(view);
+            imageViewAdvertisementBanner = (ImageView)view.findViewById(R.id.imageViewAdvertisementBanner);
+            textViewAdvertMessage = (TextView)view.findViewById(R.id.textViewAdvertMessage);
+        }
+    }
+}
---
+++ app/src/main/java/kot/android/a441recycleview/MainActivity.java
@@ -0,0 +1,191 @@
+package kot.android.a441recycleview;
+
+import androidx.annotation.IdRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ListAdapterWithRecycleView.PersonModifier{
+
+    private RecyclerView recyclerView;
+    AppUtility appUtility;
+
+    ListAdapterWithRecycleView listAdapterWithRecycleView;
+
+    private EditText editTextFirstName, editTextLastName;
+    private Spinner spinnerNationality;
+    private RadioGroup radioGroup;
+    private Button buttonAdd;
+
+    List<Person> people;
+    List<Object> catalogue;
+    int modificationIndex=-1;
+
+    String firstName, lastName, nationality;
+    Person.GENDER gender;
+
+    LinearLayoutManager linearLayoutManager;
+    GridLayoutManager gridLayoutManager;
+    StaggeredGridLayoutManager staggeredGridLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        appUtility=AppUtility.getAppUtility(getApplicationContext());
+        initPersonInputForm();
+        recyclerView =(RecyclerView) findViewById(R.id.recycleView);
+
+        people = appUtility.getPeople();
+        catalogue=appUtility.getCatalogue();
+
+        listAdapterWithRecycleView=new ListAdapterWithRecycleView(this,catalogue);
+        listAdapterWithRecycleView.setPersonModifier(this);
+
+        linearLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+
+        staggeredGridLayoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(listAdapterWithRecycleView);
+
+    }
+
+    private void initPersonInputForm(){
+        editTextFirstName = (EditText)findViewById(R.id.editTextFirstName);
+        editTextLastName = (EditText)findViewById(R.id.editTextLastName);
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroupGender);
+
+        spinnerNationality = (Spinner)findViewById(R.id.spinnerNationality);
+        spinnerNationality.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,appUtility.getUniqueNationalitiesArray()));
+        buttonAdd = (Button)findViewById(R.id.buttonAdd);
+        buttonAdd.setTag("Add");
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+                switch (i){
+                    case R.id.radioButtonMale: gender = Person.GENDER.MALE; break;
+                    case R.id.radioButtonFemale: gender = Person.GENDER.FEMALE; break;
+                    default: gender=null;
+                }
+            }
+        });
+
+        spinnerNationality.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                nationality = getResources().getStringArray(R.array.nationalities)[i];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstName = editTextFirstName.getText().toString();
+                lastName  = editTextLastName.getText().toString();
+                Person person=null;
+
+                if(isInputDataValid()) {
+                    person = new Person(firstName, lastName, gender, nationality);
+                }else{
+                    Toast.makeText(MainActivity.this,"Input Invalid",Toast.LENGTH_LONG).show();
+                }
+
+                String behaviour = (String)buttonAdd.getTag();
+                if(behaviour.equalsIgnoreCase("Add")){
+                    if(person!=null){
+                        people.add(person);
+                        listAdapterWithRecycleView.notifyDataSetChanged();
+                        recyclerView.scrollToPosition(people.size()-1);
+                        clearInputForm();
+                    }
+                }else if(behaviour.equalsIgnoreCase("modify")){
+                    if(person!=null){
+                        try{
+                            people.get(modificationIndex).setName(person.getName());
+                            people.get(modificationIndex).setLastName(person.getLastName());
+                            people.get(modificationIndex).setGender(person.getGender());
+                            people.get(modificationIndex).setNationality((String)spinnerNationality.getSelectedItem());
+
+                            listAdapterWithRecycleView.notifyItemChanged(modificationIndex);
+                            clearInputForm();
+                            buttonAdd.setTag("Add");
+                            buttonAdd.setText("Add");
+                        }catch (IndexOutOfBoundsException exception){
+                            Toast.makeText(MainActivity.this,"Can't modify, item moved",Toast.LENGTH_LONG ).show();
+                            listAdapterWithRecycleView.notifyDataSetChanged();
+                            clearInputForm();
+                            buttonAdd.setTag("Add");
+                            buttonAdd.setText("Add");
+                        }
+                    }
+                }
+
+            }
+        });
+    }
+
+    private boolean isInputDataValid(){
+        if(AppUtility.isStringEmpty(firstName) || AppUtility.isStringEmpty(lastName) || AppUtility.isStringEmpty(nationality) || gender==null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private void clearInputForm() {
+        editTextFirstName.setText("");
+        editTextLastName.setText("");
+        radioGroup.clearCheck();
+        spinnerNationality.setSelection(0);
+    }
+
+    @Override
+    public void onPersonSelected(int position) {
+        modificationIndex = position;
+        Person person=people.get(position);
+        buttonAdd.setTag("Modify");
+        buttonAdd.setText("Modify");
+
+        editTextFirstName.setText(person.getName());
+        editTextLastName.setText(person.getLastName());
+        if(person.getGender()== Person.GENDER.MALE){
+            ((RadioButton)findViewById(R.id.radioButtonMale)).performClick();
+        }else if(person.getGender()== Person.GENDER.FEMALE){
+            ((RadioButton)findViewById(R.id.radioButtonFemale)).performClick();
+        }
+        spinnerNationality.setSelection(appUtility.getNationalityForSelectedIndex(person.getNationality()));
+    }
+
+    @Override
+    public void onPersonDeleted(int position) {
+        buttonAdd.setTag("Add");
+        buttonAdd.setText("Add");
+        clearInputForm();
+    }
+
+}
